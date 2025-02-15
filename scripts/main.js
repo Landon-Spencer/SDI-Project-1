8616d60842287cce059d424feafd08e033d95cc5
Landon-Spencer <landon.r.spencer@gmail.com>
2025-02-15 14:05:56 -0700

    first image will display on site

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,58 @@
-const baseURL = 'https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&isHighlight=true&isPublicDomain=true&q=';
+const searchBaseURL = 'https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&isHighlight=true&isPublicDomain=true&q=';
+const objectIdURL = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/'
 
 document.addEventListener('DOMContentLoaded', () => {
   const searchButton = document.getElementById('search');
   const artStyle = document.getElementById('art-style-select');
+  let artIdCount = 0;
 
   searchButton.addEventListener('click', (event) => {
     event.preventDefault();
     const searchArtStyle = artStyle.value;
-    console.log('base + search: ' + baseURL + searchArtStyle);
+    console.log('base + search: ' + searchBaseURL + searchArtStyle);
     console.log(objectIdList(searchArtStyle));
     const artStyleIdList = objectIdList(searchArtStyle);
-    console.log(artStyleIdList);
-
+    // artStyleIdList.then((idArray) => console.log(idArray[0]));
+    artIdCount = 0;
+    const artistId = artStyleIdList.then((idArray) => idArray[artIdCount]);
+    artStyleIdList.then((idArray) => displayArtist(idArray, artIdCount));
+    // const currentArtObject = artistId.then((artistId) => artObjectFetch(artistId));
+    // artistId.then((artistId) => console.log('Artist Object:', artObjectFetch(artistId)));
+    // artistId.then((artistId) => displayArtist(artistId));
     // const requestedIds = objectIdFetch(searchArtStyle);
+    // var artistImage = document.createElement('img');
+    // artistImage.src = currentArtObject.primaryImage;
+    // const mainDisplay = document.getElementById('mainWrapper');
+    // mainDisplay.appendChild(artistImage);
   })
 })
+
+async function displayArtist(idArray, idCount) {
+  console.log('artStyleIdList:', idArray);
+  const artistId = idArray[idCount];
+  console.log('artistId:', artistId);
+  const currentArtObject = artObjectFetch(artistId);
+  currentArtObject.then((currentArtObject) => {
+    if (currentArtObject.primaryImage) {
+      let imageURL = currentArtObject.primaryImage
+      console.log('imageURL:', imageURL)
+      var artistImage = document.createElement('img');
+      artistImage.id = 'displayImage';
+      artistImage.maxWidth = '100%';
+      artistImage.height = '500';
+      artistImage.src = imageURL;
+      const mainDisplay = document.querySelector('.mainWrapper');
+      const oldDisplayImage = document.getElementById('displayImage');
+      mainDisplay.replaceChild(artistImage, oldDisplayImage);
+    } else {
+
+    }
+
+
+  });
+  // imageURL.then((imageURL) => console.log('imageURL:', imageURL));
+
+}
 
 async function objectIdList(searchArtStyle) {
   const idObject = await objectIdFetch(searchArtStyle);
@@ -22,7 +60,7 @@ async function objectIdList(searchArtStyle) {
 }
 
 async function objectIdFetch(requestedStyle) {
-  const rawResponse = await fetch(baseURL + requestedStyle);
+  const rawResponse = await fetch(searchBaseURL + requestedStyle);
   if (!rawResponse.ok) {
     throw new Error(`No matching data can be found for "${requestedStyle}"`);
   }
@@ -30,9 +68,9 @@ async function objectIdFetch(requestedStyle) {
 }
 
 async function artObjectFetch(objectId) {
-  const rawResponse = await fetch(baseURL + objectId);
+  const rawResponse = await fetch(objectIdURL + objectId);
   if (!rawResponse.ok) {
-    throw new Error(`No matching data can be found for "${requestedStyle}"`);
+    throw new Error(`No matching data can be found for "${objectId}"`);
   }
   return await rawResponse.json();
 }
