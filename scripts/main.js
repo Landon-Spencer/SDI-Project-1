7fec70197568a3d69005f59607381eee6dfd9c29
Landon-Spencer <landon.r.spencer@gmail.com>
2025-02-17 13:12:37 -0700

    added forward and back buttons function

--- scripts/main.js
+++ scripts/main.js
@@ -5,16 +5,21 @@ document.addEventListener('DOMContentLoaded', () => {
   const searchButton = document.getElementById('search');
   const artStyle = document.getElementById('art-style-select');
   let artIdCount = 0;
+  const searchForward = document.getElementById('search-forward');
+  const searchBack = document.getElementById('search-back');
+  let artStyleIdList;
+  let artStyleIdListLenght;
 
   searchButton.addEventListener('click', (event) => {
     event.preventDefault();
     const searchArtStyle = artStyle.value;
-    console.log('base + search: ' + searchBaseURL + searchArtStyle);
-    console.log(objectIdList(searchArtStyle));
-    const artStyleIdList = objectIdList(searchArtStyle);
+    // console.log('base + search: ' + searchBaseURL + searchArtStyle);
+    // console.log(objectIdList(searchArtStyle));
+    artStyleIdList = objectIdList(searchArtStyle);
     // artStyleIdList.then((idArray) => console.log(idArray[0]));
     artIdCount = 0;
-    const artistId = artStyleIdList.then((idArray) => idArray[artIdCount]);
+    // const artistId = artStyleIdList.then((idArray) => idArray[artIdCount]);
+    artStyleIdList.then((idArray) => artStyleIdListLenght = idArray.total);
     artStyleIdList.then((idArray) => displayArtist(idArray, artIdCount));
     // const currentArtObject = artistId.then((artistId) => artObjectFetch(artistId));
     // artistId.then((artistId) => console.log('Artist Object:', artObjectFetch(artistId)));
@@ -25,17 +30,41 @@ document.addEventListener('DOMContentLoaded', () => {
     // const mainDisplay = document.getElementById('mainWrapper');
     // mainDisplay.appendChild(artistImage);
   })
+
+  searchForward.addEventListener('click', (event) => {
+    event.preventDefault();
+    // console.log('artStyleIdList length:', artStyleIdListLenght);
+    if (artStyleIdList && artIdCount < artStyleIdListLenght) {
+      artIdCount++;
+      artStyleIdList.then((idArray) => displayArtist(idArray, artIdCount));
+    } else if (artStyleIdList && artIdCount == artStyleIdListLenght) {
+      artStyleIdList.then((idArray) => displayArtist(idArray, artIdCount));
+    }
+    console.log('artIdCount:', artIdCount);
+  })
+
+  searchBack.addEventListener('click', (event) => {
+    event.preventDefault();
+    // console.log('artStyleIdList length:', artStyleIdListLenght);
+    if (artStyleIdList && artIdCount > 0) {
+      artIdCount--;
+      artStyleIdList.then((idArray) => displayArtist(idArray, artIdCount));
+    } else if (artStyleIdList && artIdCount == 0) {
+      artStyleIdList.then((idArray) => displayArtist(idArray, artIdCount));
+    }
+    console.log('artIdCount:', artIdCount);
+  })
+
 })
 
 async function displayArtist(idArray, idCount) {
   console.log('artStyleIdList:', idArray);
-  const artistId = idArray[idCount];
-  console.log('artistId:', artistId);
+  const artistId = idArray.objectIDs[idCount];
+  // console.log('artistId:', artistId);
   const currentArtObject = artObjectFetch(artistId);
   currentArtObject.then((currentArtObject) => {
-    if (currentArtObject.primaryImage) {
       let imageURL = currentArtObject.primaryImage
-      console.log('imageURL:', imageURL)
+      // console.log('imageURL:', imageURL)
       var artistImage = document.createElement('img');
       artistImage.id = 'displayImage';
       artistImage.maxWidth = '100%';
@@ -44,19 +73,14 @@ async function displayArtist(idArray, idCount) {
       const mainDisplay = document.querySelector('.mainWrapper');
       const oldDisplayImage = document.getElementById('displayImage');
       mainDisplay.replaceChild(artistImage, oldDisplayImage);
-    } else {
-
-    }
-
-
-  });
+   });
   // imageURL.then((imageURL) => console.log('imageURL:', imageURL));
 
 }
 
 async function objectIdList(searchArtStyle) {
   const idObject = await objectIdFetch(searchArtStyle);
-  return idObject.objectIDs;
+  return idObject.data;
 }
 
 async function objectIdFetch(requestedStyle) {
@@ -64,7 +88,8 @@ async function objectIdFetch(requestedStyle) {
   if (!rawResponse.ok) {
     throw new Error(`No matching data can be found for "${requestedStyle}"`);
   }
-  return await rawResponse.json();
+  const result = await rawResponse.json();
+  return {data: result};
 }
 
 async function artObjectFetch(objectId) {
